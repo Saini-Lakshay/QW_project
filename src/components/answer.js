d2d6bb91aa403eb3d0bf3784c78cec06769b9986
Lakshay Saini <44637081+Saini-Lakshay@users.noreply.github.com>
2020-06-08 18:49:37 +0530

    filter added

--- src/components/answer.js
+++ src/components/answer.js
@@ -9,7 +9,12 @@ class Answer extends Component {
     constructor(props) {
         super(props);
 
+        this.onChangefilter = this.onChangefilter.bind(this);
+        this.quesList = this.quesList;
+
+
         this.state = {
+            filter: '',
             subjectName: '',
             question: '',
             exercises: []
@@ -35,9 +40,37 @@ class Answer extends Component {
         this.fetchData();
     }
 
+    onChangefilter(e) {
+        this.setState({
+            filter: e.target.value
+        });
+    }
+
     quesList() {
         return this.state.exercises.map(currentExercise => {
-            return <tr>
+            if (this.state.filter === '')
+                return <tr key={currentExercise._id}>
+                    <td className="ques-1">{currentExercise.subjectName}</td>
+                    <td className="ques-2">{currentExercise.question}</td>
+                    <td className="ques-3"><Link to={"/exercise/answer2/" + currentExercise._id} className="ques-3-lnk">Answer</Link><br></br>
+                        <Link to={"/exercise/allAnswers/" + currentExercise._id} className="ques-3-lnk">All Answers</Link>
+                    </td>
+                </tr >
+            else
+                if (currentExercise.subjectName == this.state.filter)
+                    return <tr key={currentExercise._id}>
+                        <td className="ques-1">{currentExercise.subjectName}</td>
+                        <td className="ques-2">{currentExercise.question}</td>
+                        <td className="ques-3"><Link to={"/exercise/answer2/" + currentExercise._id} className="ques-3-lnk">Answer</Link><br></br>
+                            <Link to={"/exercise/allAnswers/" + currentExercise._id} className="ques-3-lnk">All Answers</Link>
+                        </td>
+                    </tr >
+        })
+    }
+
+    /*filterres() {
+        return this.state.exercises.map(currentExercise => {
+            return <tr key={currentExercise._id}>
                 <td className="ques-1">{currentExercise.subjectName}</td>
                 <td className="ques-2">{currentExercise.question}</td>
                 <td className="ques-3"><Link to={"/exercise/answer2/" + currentExercise._id} className="ques-3-lnk">Answer</Link><br></br>
@@ -45,7 +78,7 @@ class Answer extends Component {
                 </td>
             </tr >
         })
-    }
+    }*/
 
 
     render() {
@@ -53,6 +86,7 @@ class Answer extends Component {
             return (
                 <div className="container parent-ans">
                     <h1 className="parent-h">See Answers / Add Your Answer .....</h1><hr className="parent-hr"></hr>
+                    <label>Enter subject here to Search: </label><input type="text" placeholder="Enter here..." value={this.setState.filter} onChange={this.onChangefilter} />
                     <table className="ques-tab">
                         <thead>
                             <tr>
